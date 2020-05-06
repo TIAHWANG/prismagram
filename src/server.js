8897e851c3f8a7f6bebea94a1606ffc39a373f95
Tia Hwang <tiajshwang@gmail.com>
2020-05-06 15:43:11 +0900

    Setup

---
+++ src/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+
+// Add middlewares
+server.express.use(logger("dev"));
+
+server.start({ port: PORT }, () => console.log(`Server running on http://localhost:${PORT}`));
