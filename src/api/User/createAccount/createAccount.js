8852403b42789b80252dea6a32747f2fc4745391
Tia Hwang <tiajshwang@gmail.com>
2020-05-14 15:10:25 +0900

    Refactoring: Authorization parts

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,6 +4,12 @@ export default {
     Mutation: {
         createAccount: async (_, args) => {
             const { username, email, firstName = "", lastName = "", bio = "" } = args;
+            const exists = await prisma.$exists.user({
+                OR: [{ username }, { email }],
+            });
+            if (exists) {
+                throw Error("This username / email is already taken.");
+            }
             const user = await prisma.createUser({
                 username,
                 email,
@@ -11,7 +17,7 @@ export default {
                 lastName,
                 bio,
             });
-            return user;
+            return true;
         },
     },
 };
--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -4,11 +4,9 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
     Mutation: {
         requestSecret: async (_, args, { request }) => {
-            console.log(request);
             const { email } = args;
             const loginSecret = generateSecret();
             try {
-                throw Error();
                 await sendSecretMail(email, loginSecret);
                 await prisma.updateUser({ data: { loginSecret }, where: { email } });
                 return true;
