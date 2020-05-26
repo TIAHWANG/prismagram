cb3d6e23e98fba6acdc691e327809ac772dd67ba
Tia Hwang <tiajshwang@gmail.com>
2020-05-26 18:55:28 +0900

    Add: followingCount+followersCount+postsCount

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -3,6 +3,27 @@ import { prisma } from "../../../generated/prisma-client";
 export default {
     User: {
         // parent: 위에 존재하는 resolver의 user 정보를 담고 있음
+        posts: ({ id }) => prisma.user({ id }).posts(),
+        following: ({ id }) => prisma.user({ id }).following(),
+        followers: ({ id }) => prisma.user({ id }).followers(),
+        likes: ({ id }) => prisma.user({ id }).likes(),
+        comments: ({ id }) => prisma.user({ id }).comments(),
+        rooms: ({ id }) => prisma.user({ id }).rooms(),
+        followingCount: ({ id }) =>
+            prisma
+                .usersConnection({ where: { following_some: { id } } })
+                .aggregate()
+                .count(),
+        followersCount: ({ id }) =>
+            prisma
+                .usersConnection({ where: { followers_some: { id } } })
+                .aggregate()
+                .count(),
+        postsCount: ({ id }) =>
+            prisma
+                .postsConnection({ where: { user: { id } } })
+                .aggregate()
+                .count(),
         fullName: (parent) => {
             return `${parent.firstName} ${parent.lastName}`;
         },
